1ee6b2f9425ba58a0d57f831b28f6f5698c88ebb
Sergey Rodionov <rodionov.sergey.v@gmail.com>
2017-11-21 18:31:08 +0300

    php send UTF encoding

--- send.php
+++ send.php
@@ -7,11 +7,12 @@
 
     // Create the email and send the message  echo "No arguments Provided!";
 
-    $to = "hi@sdfinance.ru, leirbythe@gmail.com";
+    $headers = "Content-Type: text/html; charset=UTF-8";
+    $to = "hi@sdfinance.ru";
     $subject = "Форма обратной связи от: $email_address $phone";
     $message = "Вы получили новое сообщение через форму обратной связи.\n\n"."Отправитель: $email_address\n\nИмя: $name\n\nДата рождения: $date\n\nСообщение: $message_email";
 
-    var_dump(mail($to,$subject,$message));
+    var_dump(mail($to,$subject,$message,$headers));
 
     return true;
 ?>
