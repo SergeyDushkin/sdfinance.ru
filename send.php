d89a439f11ce96ea7cf5a2d7c1d6b525d74af0b4
Sergey Rodionov <rodionov.sergey.v@gmail.com>
2017-11-30 13:28:45 +0300

    telegram bot added

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php   
     $name = strip_tags(htmlspecialchars($_POST['name']));
     $date = strip_tags(htmlspecialchars($_POST['date']));
-    $email_address = strip_tags(htmlspecialchars($_POST['email']));
+//    $email_address = strip_tags(htmlspecialchars($_POST['email']));
     $phone = strip_tags(htmlspecialchars($_POST['phone']));
     $message_email = strip_tags(htmlspecialchars($_POST['message']));
 
@@ -16,11 +16,29 @@
     $subject = $now->format('Y-m-d H:i:s');
     $message = "Имя: " . $name . "<br/>" .	
 		"Телефон: " . $phone . "<br/>" . 
-		"E-mail: " . $email_address . "<br/>" .
-		"Дата рождения: " . $date . "<br/>" .
+		"Год рождения: " . $date . "<br/>" .
 		"Сообщение: " . $message_email . "<br/>";
 
     var_dump(mail($to,$subject,$message,$headers));
 
-    return true;
+	// send message from Telegram bot
+	$token = "330835026:AAF_5l8PMe7vxq-H-zn28BFucoLhAyCZklQ";
+	$chatid = "312593152";
+
+    	$message = "Имя: " . $name . "\n" .	
+		"Телефон: " . $phone . "\n" . 
+		"Год рождения: " . $date . "\n" .
+		"Сообщение: " . $message_email . "\n";
+
+	$url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatid . "&text=" . urlencode($message);
+	$ch = curl_init();
+	$optArray = array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true
+    	);
+    	curl_setopt_array($ch, $optArray);
+    	$result = curl_exec($ch);
+    	curl_close($ch);
+
+	return true;
 ?>
