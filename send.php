e78f8f3ff65f348fdf6909e6a82bc507e07337da
Sergey Rodionov <rodionov.sergey.v@gmail.com>
2018-03-27 22:17:51 +0300

    send.php

--- send.php
+++ send.php
@@ -7,14 +7,15 @@
     $phone = strip_tags(htmlspecialchars($_POST['phone']));
     $message_email = strip_tags(htmlspecialchars($_POST['message']));
 
+    $now = new DateTime();
+    $now->setTimezone($tz_object);
+    
     // Create the email and send the message  echo "No arguments Provided!";
-
+/*
     $headers = "Content-Type: text/html; charset=UTF-8";
     $to = "hi@sdfinance.ru";
 
     $tz_object = new DateTimeZone('Europe/Moscow');
-    $now = new DateTime();
-    $now->setTimezone($tz_object);
     $subject = $now->format('Y-m-d H:i:s');
     $message = "Имя: " . $name . "<br/>" .	
 		"Телефон: " . $phone . "<br/>" . 
@@ -22,12 +23,12 @@
 		"Сообщение: " . $message_email . "<br/>";
 
     var_dump(mail($to,$subject,$message,$headers));
-
+*/
 	// send message from Telegram bot
 	$token = "330835026:AAF_5l8PMe7vxq-H-zn28BFucoLhAyCZklQ";
 	$chatid = "312593152";
 
-    	$message = "Имя: " . $name . "\n" .	
+	$message = "Имя: " . $name . "\n" .	
 		"Телефон: " . $phone . "\n" . 
 		"Год рождения: " . $date . "\n" .
 		"Сообщение: " . $message_email . "\n";
